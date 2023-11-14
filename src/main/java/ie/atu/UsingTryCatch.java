3238278d62a340c0d158be1ed620b73a1454add4
G00422935@atu.ie <G00422935@atu.ie>
2023-11-14 13:35:59 +0000

    Starting Using Try Catch

--- src/main/java/ie/atu/UsingTryCatch.java
+++ src/main/java/ie/atu/UsingTryCatch.java
@@ -5,5 +5,20 @@ import java.util.Scanner;
 public class UsingTryCatch {
     public static void main(String[] args) {
 
+        System.out.println("Please enter a number between 0 - 9 :");
+        Scanner myscan =  new Scanner(System.in);
+
+        // Read user input as a String
+        String input = myscan.nextLine();
+
+        //Parse the input to an integer
+        int myNum = Integer.parseInt(input);
+
+        if (myNum >= 0 && myNum <= 9){
+            System.out.println("You entered " + myNum);
+        }
+        else{
+            System.out.println("Not a valid number");
+        }
     }
 }
