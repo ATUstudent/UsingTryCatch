4d7ae4bdbcfc23ca7cf8c0c419f6dd9c47c4191b
G00422935@atu.ie <G00422935@atu.ie>
2023-11-14 13:54:38 +0000

    allow user to continue after entering invalid char

--- src/main/java/ie/atu/UsingTryCatch.java
+++ src/main/java/ie/atu/UsingTryCatch.java
@@ -4,21 +4,28 @@ import java.util.Scanner;
 
 public class UsingTryCatch {
     public static void main(String[] args) {
+        while(true) {
 
-        System.out.println("Please enter a number between 0 - 9 :");
-        Scanner myscan =  new Scanner(System.in);
+            try {
 
-        // Read user input as a String
-        String input = myscan.nextLine();
+                System.out.println("Please enter a number between 0 - 9 :");
+                Scanner myscan = new Scanner(System.in);
 
-        //Parse the input to an integer
-        int myNum = Integer.parseInt(input);
+                // Read user input as a String
+                String input = myscan.nextLine();
 
-        if (myNum >= 0 && myNum <= 9){
-            System.out.println("You entered " + myNum);
-        }
-        else{
-            System.out.println("Not a valid number");
+                //Parse the input to an integer
+                int myNum = Integer.parseInt(input);
+
+                if (myNum >= 0 && myNum <= 9) {
+                    System.out.println("You entered " + myNum);
+                    break;
+                } else {
+                    System.out.println("Not a valid number");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Error, enter a numeric value");
+            }
         }
     }
 }
